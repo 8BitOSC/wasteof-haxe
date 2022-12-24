12d33b2112574865be682de211145ae61a41fe7f
8BitJake <jake8bittaylor@gmail.com>
2022-12-24 17:53:43 -0600

    svg image support + html5 unsupported errors

--- wasteofhaxe/WasteOfHaxe2.hx
+++ wasteofhaxe/WasteOfHaxe2.hx
@@ -1,18 +1,23 @@
 package wasteofhaxe;
 
+#if !html5
 import haxe.Http;
 import haxe.io.Bytes;
 import haxe.Json;
 import wasteofhaxe.Typedefs;
 import openfl.display.BitmapData;
+import openfl.utils.ByteArray;
+import haxe.Exception;
 
 using StringTools;
 
 class WasteOfHaxe2 {
 	public static var apiUrl:String = 'https://api.wasteof.money/';
+
 	private static function makeRequest(url:String = '', isJson:Bool = true):Dynamic {
-		var h:Http = new Http(apiUrl+url);
-        h.setHeader("User-Agent", "request");
+		trace(apiUrl + url);
+		var h:Http = new Http(apiUrl + url);
+		h.setHeader("User-Agent", "request");
 		var r:Null<Dynamic> = null;
 		h.onData = function(d) {
 			r = d;
@@ -27,10 +32,18 @@ class WasteOfHaxe2 {
 		return r;
 	}
 
-	private static function makeBitmapRequest(url:String):BitmapData {
-		var h:Http = new Http(apiUrl+url);
+
+	private static function makeBitmapRequest(url:String):Null<BitmapData> {
+		var h:Null<Http> = null;
+		var svgUrl:String = 'https://wasteofhaxe-image-handler.8bitjake.repl.co/?url=$apiUrl$url';
+		if(Http.requestUrl(svgUrl) == 'is already png'){
+			h = new Http(apiUrl + url);
+		} else {
+			h = new Http(svgUrl);
+		}
 		h.setHeader("User-Agent", "request");
 		var r:Null<ByteArray> = null;
+
 		h.onBytes = function(d) {
 			r = d;
 		};
@@ -70,11 +83,24 @@ class WasteOfHaxe2 {
 		return makeRequest('users/$user/followers/$other');
 	}
 
-	public static function getProfilePicture(user:String):BitmapData {
+	public static function getProfilePicture(user:String):Null<BitmapData> {
 		return makeBitmapRequest('users/$user/picture');
 	}
 
-	public static function getBanner(user:String):BitmapData {
+	public static function getBanner(user:String):Null<BitmapData> {
 		return makeBitmapRequest('users/$user/banner');
 	}
 }
+#else
+
+class WasteOfHaxe2 { // same class, but all functions do nothing
+	public static function getHome(){}
+	public static function getUser(user:String){}
+	public static function getPosts(user:String, page:Int = 1){}
+	public static function getFollowing(user:String, page:Int = 1){}
+	public static function getFollowers(user:String, page:Int = 1){}
+	public static function isFollowing(user:String, other:String){}
+	public static function getProfilePicture(user:String){}
+	public static function getBanner(user:String){}
+}
+#end
